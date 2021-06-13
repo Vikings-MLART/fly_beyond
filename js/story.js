f2aabbf6384a7f4604c83d13175fce94943b126c
leen-duafi <leen-hala@hotmail.com>
2021-06-14 00:11:39 +0300

    ok

--- js/story.js
+++ js/story.js
@@ -1,51 +1,70 @@
 'use strict';
 //Random user images for https://randomuser.me/
 let randomUserImageURL;
-const generateRandomUser =function(userGender){
-  return new Promise(function(resolve){
+const generateRandomUser = function (userGender) {
+  return new Promise(function (resolve) {
     fetch('https://randomuser.me/api/?results=1')
       .then(res => res.json())
       .then(data => {
-        if(data.results[0].gender === userGender)
-          return resolve( data.results[0].picture.large);
-        else{
+        if (data.results[0].gender === userGender)
+          return resolve(data.results[0].picture.large);
+        else {
           return resolve(generateRandomUser(userGender));
         }
       });
   });
 };
-async function storySubmtionHandler(event){
+async function storySubmtionHandler(event) {
   event.preventDefault();
   const userName = event.target.name.value;
   const usetStoryText = event.target.description.value;
   const userGender = event.target.gender.value;
   randomUserImageURL = await generateRandomUser(userGender);
-  new Story(userName, usetStoryText, randomUserImageURL);
-  localStorage.setItem('userStories',JSON.stringify(Story.storiesList));
+  const picArray = event.target.pictuers.value.split(' ');
+  new Story(userName, usetStoryText, randomUserImageURL, picArray);
+  localStorage.setItem('userStories', JSON.stringify(Story.storiesList));
   renderStories();
   event.target.reset();
+  // const storyUserGallary = document.createElement('div');
+  // const pic = document.getElementById('pictuers');
+  // if (pic.value) {
+  //   const picArray = pic.value.split(' ');
+  //   for (let i = 0; i < picArray.length; i++) {
+  //     let appear = document.createElement('img');
+  //     appear.setAttribute("src", picArray[i]);
+  //     storyUserGallary.appendChild(appear);
+  //   }
+
+  // }
 }
-function Story(name, storyText, imgPath) {
+function Story(name, storyText, imgPath, picArray) {
   this.name = name;
   this.imgPath = imgPath;
   this.storyText = storyText;
+  this.picArray = picArray;
   Story.storiesList.push(this);
 }
 Story.storiesList = [];
-const clearStories = function(){
+const clearStories = function () {
   document.querySelector('.users-stories-container').innerHTML = '';
 };
-const loadStorage = function(){
+const loadStorage = function () {
   Story.storiesList = JSON.parse(localStorage.getItem('userStories')) || [];
 };
-const renderStories = function(){
-  console.log('out');
+
+
+const storyTextContainer = document.createElement('div');
+const storyUserGallary = document.createElement('div');
+
+
+const renderStories = function () {
+
   clearStories();
   loadStorage();
   const userStoriesContainer = document.querySelector('.users-stories-container');
-  console.log(Story.storiesList.length);
-  for(let i = Story.storiesList.length - 1; i >= 0; i--){
-    console.log('in');
+
+  for (let i = Story.storiesList.length - 1; i >= 0; i--) {
+
     const storyElement = document.createElement('div');
     const storyTextContainer = document.createElement('div');
     const userName = document.createElement('h3');
@@ -60,8 +79,25 @@ const renderStories = function(){
     storyTextContainer.appendChild(userStoryText);
     storyElement.appendChild(userImage);
     storyElement.appendChild(storyTextContainer);
+    if (Story.storiesList[i].picArray.length) {
+      const storyUserGallary = document.createElement('div');
+      for (let j = 0; j < Story.storiesList[i].picArray.length; j++) {
+        let appear = document.createElement('img');
+        appear.setAttribute("src", Story.storiesList[j].picArray);
+        storyUserGallary.appendChild(appear);
+
+      }
+      storyElement.appendChild(storyUserGallary);
+    }
+
+
+
+    
     userStoriesContainer.appendChild(storyElement);
+
   }
 };
 document.querySelector('.story-form').addEventListener('submit', storySubmtionHandler);
 renderStories();
+
+
